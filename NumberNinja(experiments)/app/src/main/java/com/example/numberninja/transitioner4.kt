3fa7ca56aff7cd1299c1c8109cc3d654f551fae8
sfbueno <bueno.shawnfilbert@ue.edu.ph>
2023-12-08 11:49:00 +0800

    updates

---
+++ NumberNinja(experiments)/app/src/main/java/com/example/numberninja/transitioner4.kt
@@ -0,0 +1,21 @@
+package com.example.numberninja
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class transitioner4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transitioner4)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, expertLVL::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000) // Delay for 2 seconds (adjust as needed)
+    }
+}
